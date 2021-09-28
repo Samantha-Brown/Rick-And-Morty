6e0714866b46ea34bff4d15915433c0c8b6b5fb5
Samantha Brown <bro7445@gmail.com>
2021-09-27 21:59:26 -0400

    Remove unused error component files

--- cypress/integration/main_page.js
+++ cypress/integration/main_page.js
@@ -3,11 +3,16 @@ describe('User flows via main page', () => {
     cy.visit('http://localhost:3000/');
   });
 
-  it('Should confirm that true is equal to true', () => {
-     expect(true).to.equal(true)
-   })
-   
-  it('should show the title', () => {
-  cy.get('header')
-})
+    it('Should confirm that true is equal to true', () => {
+       expect(true).to.equal(true)
+     });
+
+    it('should show the title', () => {
+    cy.get('header')
+  });
+
+    it('Should be able to click on start button and page updates URL and displays questions', () => {
+        cy.get('.start-button').click()
+        cy.url().should('not.eq', 'http://localhost:3000')
+    });
 })
