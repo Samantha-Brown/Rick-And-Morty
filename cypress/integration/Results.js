373a30db94706bb3e00cf048db9f3ebec6f56db4
Samantha Brown <bro7445@gmail.com>
2021-09-27 21:22:03 -0400

    Add the connection test to all cypress files

--- cypress/integration/Results.js
+++ cypress/integration/Results.js
@@ -1,7 +1,13 @@
-describe('User flows via results page'), () => {
+describe('User flows via results page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/Results');
   });
 
-  it('')
-}
+  it('Should confirm that true is equal to true', () => {
+     expect(true).to.equal(true)
+   })
+   
+  it('should show the title', () => {
+  cy.get('header')
+})
+})
--- cypress/integration/main_page.js
+++ cypress/integration/main_page.js
@@ -3,6 +3,10 @@ describe('User flows via main page', () => {
     cy.visit('http://localhost:3000/');
   });
 
+  it('Should confirm that true is equal to true', () => {
+     expect(true).to.equal(true)
+   })
+   
   it('should show the title', () => {
   cy.get('header')
 })
